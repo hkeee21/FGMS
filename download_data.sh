e434205c470b546e3e1c2ca697686d6f91973480
hkeee21 <hjkl1379991@126.com>
2023-04-19 03:40:34 +0000

    [update] shell file to download data

---
+++ download_data.sh
@@ -0,0 +1,9 @@
+wget https://cloud.tsinghua.edu.cn/f/ceb16d6040b8498aa710/?dl=1
+mv 'index.html?dl=1' sample-data.zip
+unzip sample-data.zip
+rm -r __MACOSX
+rm sample-data.zip
+
+# please access https://cloud.tsinghua.edu.cn/f/ceb16d6040b8498aa710/
+# or https://drive.google.com/file/d/1l5kX65IUn3xQ8_Wszy96hhHgJe0S_XIm/view?usp=share_link
+# if the shell file does not work for you
